ef7a62b1076fe5a679b5137892b9a91cac4793c7
pradipk1 <pradipshukla12699@gmail.com>
2024-03-17 19:01:31 +0530

    mongodb atlas connected

--- database/connectDatabase.js
+++ database/connectDatabase.js
@@ -2,8 +2,11 @@
 const mongoose = require('mongoose');
 
 
+const mongodbAtlasConnectionUrl = 'mongodb+srv://socialMediaUser:socialMediaUser123@cluster0.ysnmozx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
 async function connectDatabase() {
-    return await mongoose.connect('mongodb://127.0.0.1:27017/social-media');
+    // return await mongoose.connect('mongodb://127.0.0.1:27017/social-media');
+    return await mongoose.connect(mongodbAtlasConnectionUrl);
 }
 
 
